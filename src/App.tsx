2cd943965201bad4084238e601a818612a7588d9
Krb0 <maurodavidcaceres@gmail.com>
2022-04-26 21:28:42 -0300

    remove icon added

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,14 @@ import Images from "./components/Images";
 import Uploader from "./components/Uploader";
 function App() {
   const [images, setImages] = useState<string[]>([]);
-
+  const handleRemove = (file: string) => {
+    const newImages = [...images];
+    newImages.splice(newImages.indexOf(file), 1);
+    setImages(newImages);
+  };
   return (
     <div className="app">
-      <Images images={images} />
+      <Images images={images} handleRemove={handleRemove} />
       <Uploader setImages={setImages} images={images} />
     </div>
   );
--- src/components/Images/index.tsx
+++ src/components/Images/index.tsx
@@ -1,8 +1,23 @@
-const Images = ({ images }: { images: string[] }) => {
+const Images = ({
+  images,
+  handleRemove,
+}: {
+  images: string[];
+  handleRemove: (file: string) => void;
+}) => {
   return (
     <div className="image-container">
       {images.map((file: string, i) => (
-        <img src={file} alt="uploaded" key={file} />
+        <div key={file}>
+          <span className="image-overlay" onClick={() => handleRemove(file)}>
+            <img
+              src="https://iconarchive.com/download/i58687/dakirby309/windows-8-metro/Folders-OS-Recycle-Bin-Full-Metro.ico"
+              className="remove-icon"
+              alt="remove"
+            />
+          </span>
+          <img src={file} alt="uploaded" />
+        </div>
       ))}
     </div>
   );
